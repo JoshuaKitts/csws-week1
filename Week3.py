894092a93771b4522e7c6bf9d58fcb18dd09e3e8
“Joshua <joshuakitts11@gmail.com>
2023-02-10 11:09:37 +0000

    changes

---
+++ Week3.py
@@ -0,0 +1,26 @@
+numbers1 = list(range(1, 1000001))
+
+#print(numbers1)
+
+print({sum(numbers1)})
+
+######
+
+numbers2 = list(range(1, 21, 2))
+
+print(numbers2)
+
+######
+
+numbers3 = list(range(3, 31, 3))
+
+print(numbers3)
+
+######
+
+squares = [x**2 for x in range(1, 11)]
+
+print(squares)
+
+######
+
